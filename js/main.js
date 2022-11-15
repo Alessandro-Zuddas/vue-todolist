097fda41e2821972a2970b1e898cbdecd7c67eb6
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-11-15 14:58:21 +0100

    Aggiunge: rimozione task, task done al click

--- js/main.js
+++ js/main.js
@@ -7,21 +7,39 @@ createApp({
   data() {
     return {
 
-      message: 'Hello Vue!'
+        newTask: "",
+
+        toDoList: [
+
+        {
+            taskName: "Fare la spesa",
+            wasDone: false
+        },
+        {
+            taskName: "Fare l'esercizio",
+            wasDone: false
+        }
+
+      ],
 
     }
+  },
+  methods: {
+    taskDone(index){
+        if(this.toDoList[index].wasDone === false){
+            this.toDoList[index].wasDone = true;
+        }else{
+            this.toDoList[index].wasDone = false;
+        }
+    },
+    removeTask(index){
+        this.toDoList.splice(index, 1);
+    }
   }
-  
+
 }).mount('#app')
 
 //Descrizione:
-// Rifare l'esercizio della to do list.
-// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-// - text, una stringa che indica il testo del todo
-// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all'interno di una lista, un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 // MILESTONE 3
